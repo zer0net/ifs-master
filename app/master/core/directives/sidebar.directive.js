6d3a1943f876fdb734a9b63df7764be80cf48bd3
david nelband <dnelband@gmail.com>
2017-02-28 19:30:08 +0100

    channel logo shown in channels sidebar list

--- app/master/core/directives/sidebar.directive.js
+++ app/master/core/directives/sidebar.directive.js
@@ -4,6 +4,11 @@ app.directive('sidebar', ['$rootScope',
 		// header directive controller
 		var controller = function($scope,$element) {
 			
+			// render channel item
+			$scope.renderChannelItem = function(channel){
+				channel.logo_path = '/'+$scope.page.site_info.address+'/merged-'+$scope.page.site_info.content.merger_name + '/' + channel.cluster_id + '/data/users/' + channel.user_id + '/' + channel.logo_file;
+			};
+
 			// on filter channel
 			$scope.onFilterChannel = function(channel) {
 				// on filter channel
@@ -23,10 +28,10 @@ app.directive('sidebar', ['$rootScope',
 							       '<li class="sidebar-brand">' +
 							          'Channels  <span class="glyphicon glyphicon-refresh"  ng-click="onRemoveFilterChannel()" ></span>' +
 							        '</li>' +
-							       	'<li ng-repeat="channel in channels | orderBy:\'-date_added\'" ng-click="onFilterChannel(channel)">' +
+							       	'<li ng-repeat="channel in channels | orderBy:\'-date_added\'" ng-init="renderChannelItem(channel)" ng-click="onFilterChannel(channel)">' +
 							            '<div>' +
 							                '<a href="#">' +
-												'<figure class="channel-list-item-logo"><img ng-if="channel.logo" ng-src="/{{page.site_info.address}}/merged-{{merger_name}}/{{channel.channel_address}}/uploads/images/{{channel.logo}}"/></figure>' +
+												'<figure class="channel-list-item-logo"><img ng-if="channel.logo_file" ng-src="{{channel.logo_path}}"/></figure>' +
 							                	'<span>{{channel.channel_name}} [{{channel.items_total}}]</span>' +
 							                '</a>' +
 							            '</div>' +
