592bdcbe4c31f8a9c71f3eda12d7ca94cca7633f
siyuan dong <dongbeibei@hotmail.com>
2017-03-01 12:40:33 +0100

    updates

--- app/master/core/directives/site-header.directive.js
+++ app/master/core/directives/site-header.directive.js
@@ -126,8 +126,9 @@ app.directive('siteHeader', ['$mdDialog', '$mdMedia','$rootScope','$location',
 		  						'<site-search ng-if="!loading"></site-search>' +
 		  						'<ul class="nav navbar-nav navbar-right">'+			  						
 			  						'<li><a ng-click="showInfoModal(ev)" >FAQ</a></li>'+  
+			  						'<li><a href="/{{page.site_info.address}}/user/">My Channels</a></li>'+  
 									'<li><a href="/{{page.site_info.address}}/register.html">All Channels</button></a></li>'+  												
-									'<li><a href="/{{page.site_info.address}}/user/">My Channels</a></li>'+  
+									
 								'</ul>'+    					
 	  						'</div>' +
 	  					'</div>'+
