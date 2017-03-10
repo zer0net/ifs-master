e392da0a005b33fbda098cc52b6a43230b153b6f
siyuan dong <dongbeibei@hotmail.com>
2017-03-10 11:34:19 +0100

    updates

--- app/master/core/directives/channel-register-list.directive.js
+++ app/master/core/directives/channel-register-list.directive.js
@@ -5,17 +5,20 @@ app.directive('channelRegisterList', ['$location','$rootScope',
 		var controller = function($scope,$element) {
 			$scope.itemsPerPage = 10;
 			$scope.sortKey = 'date_added';
-			$scope.reverse = true;			
-		    
-		    console.log("-------------------------------------");
-		    console.log($scope.channels);
+			$scope.reverse = true;	
+			$scope.query=null;		
+		    		    
 		    // on filter channel
 		    $scope.onFilterChannel = function(channel) {
 		    	// on filter channel
 		    	$rootScope.$broadcast('onFilterChannel',channel);
 		    };
 
-
+		    // on filter cluster
+		    $scope.onFilterCluster = function(cluster) {
+		    	// on filter cluster		    	
+		    	$scope.query = {cluster_id:cluster.cluster_id};		    	
+		    };
 			// sort file list
 			$scope.sort = function(keyname){
 				if($scope.sortKey==keyname) {
@@ -28,13 +31,17 @@ app.directive('channelRegisterList', ['$location','$rootScope',
 		    };		   
 		};
 
-		var template=  '<div class="container-fluid" id="channel-register-list">' +			
+		var template=  '<div class="container" id="channel-register-list">' +								
+							'<ul class="item-clusters">' +	
+								'<li><h3>Clusters</h3></li>' +							
+								'<li ng-repeat="c in clusters"><a class="selected" ng-bind="c.title" ng-click="onFilterCluster(c)"></a></li>' +							
+							'</ul>' +
 							'<div class="section-header">'+
 								'<h2>Channels</h2>'+
 								'<span class="label" ng-hide="loading">total: <span ng-bind="channels.length"></span></span>'+
 								'<hr/>'+
 							'</div>'+				
-							'<table class="table table-striped table-hover">' +
+							'<table class="table table-striped table-hover table-sm">' +
 								'<thead>' +
 									'<tr>' +
 										'<th ng-click="sort(\'channel_name\')">Name<span class="glyphicon sort-icon" ng-show="sortKey==\'channel_name\'" ng-class="{\'glyphicon-chevron-down\':reverse,\'glyphicon-chevron-up\':!reverse}"></span></th>' +										
@@ -47,12 +54,12 @@ app.directive('channelRegisterList', ['$location','$rootScope',
 										'<th ng-if="page.site_info.settings.own">Action</th>' +
 									'</tr>' +
 								'</thead>' +
-								'<tr dir-paginate="channel in channels|orderBy:sortKey:reverse|itemsPerPage:itemsPerPage track by $index" pagination-id="channel-register-list-pid" moderations ng-if="channel.hide !== 1 || channel.hide === 1 && page.site_info.settings.own">' +
+								'<tr dir-paginate="channel in channels|orderBy:sortKey:reverse | filter:query |itemsPerPage:itemsPerPage track by $index" pagination-id="channel-register-list-pid" moderations ng-if="channel.hide !== 1 || channel.hide === 1 && page.site_info.settings.own">' +
 									'<td><a ng-click="onFilterChannel(channel)">{{channel.channel_name}}</a></td>' +									
 									'<td>{{channel.channel_description }}</td>' +
 									'<td><span am-time-ago="channel.date_added"></span></td>' +
 									'<td>{{channel.items.audios.length}}</td>' +
-									'<td>{{channel.items.books.length}}</td>' +
+									'<td>{{(channel.items.books.length==0)?"":channel.items.books.length}}</td>' +
 									'<td>{{channel.items.games.length}}</td>' +
 									'<td>{{channel.items.videos.length}}</td>' +
 									'<td ng-if="page.site_info.settings.own" >'+
